7c5cdc8fb9ac27c70b4428d1c75aa683b3c7a192
Jorge Esqueda Rocha <jorge_squiky@hotmail.com>
2026-05-29 18:07:38 -0600

    Switch to sqlite database

--- database.py
+++ database.py
@@ -2,11 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Connecting to SQLite
-# SQLACHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
-# engine = create_engine(SQLACHEMY_DATABASE_URL, connect_args={ 'check_same_thread': False })
+SQLACHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+engine = create_engine(SQLACHEMY_DATABASE_URL, connect_args={ 'check_same_thread': False })
 
-SQLACHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost/TodoApplicationDatabase'
-engine = create_engine(SQLACHEMY_DATABASE_URL)
+# SQLACHEMY_DATABASE_URL = 'postgresql://postgres:admin@localhost/TodoApplicationDatabase'
+# engine = create_engine(SQLACHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
